fcede80cea67be46a349b00a5830d23056a38430
Brady Svedin <brady@lightningkite.com>
2019-10-17 20:09:10 -0600

    Added with write observable and bindSelectedNullable to compoundButton

--- android/src/main/java/com/lightningkite/kwift/observables/actual/CompoundButton.binding.kt
+++ android/src/main/java/com/lightningkite/kwift/observables/actual/CompoundButton.binding.kt
@@ -19,6 +19,20 @@ fun <T> CompoundButton.bindSelect(value: T, observable: MutableObservablePropert
 
 }
 
+fun <T> CompoundButton.bindSelectNullable(value: T, observable: MutableObservableProperty<T?>) {
+    observable.addAndRunWeak(this) { self, it ->
+        self.isChecked = it == value
+    }
+    setOnCheckedChangeListener { buttonView, isChecked ->
+        if (isChecked) {
+            observable.value = value
+        } else {
+            observable.value = null
+        }
+    }
+
+}
+
 fun CompoundButton.bind(observable: MutableObservableProperty<Boolean>) {
     observable.addAndRunWeak(this) { self, it ->
         if (it != this.isChecked) {
