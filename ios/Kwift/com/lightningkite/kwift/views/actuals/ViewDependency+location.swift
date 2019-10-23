6b4103618a62b83e38b2b3645c9c0154afb9f38a
Joseph Ivie <josephivie@gmail.com>
2019-10-23 12:12:39 -0600

    Location from ViewDependency

---
+++ ios/Kwift/com/lightningkite/kwift/views/actuals/ViewDependency+location.swift
@@ -0,0 +1,60 @@
+//
+//  ViewDependency+location.swift
+//  Alamofire
+//
+//  Created by Joseph Ivie on 10/23/19.
+//
+
+import UIKit
+import CoreLocation
+
+
+public extension ViewDependency {
+    func requestLocation(_ accuracyBetterThanMeters: Double, _ onResult: @escaping (LocationResult)->Void) {
+        requestLocation(accuracyBetterThanMeters: accuracyBetterThanMeters, onResult: onResult)
+    }
+    func requestLocation(accuracyBetterThanMeters: Double, onResult: @escaping (LocationResult)->Void) {
+        let manager = CLLocationManager()
+        manager.requestWhenInUseAuthorization()
+        let retainId = "locationManager" + String(arc4random() % 10000)
+        self.parentViewController.retain(as: retainId, item: manager)
+        let sd = ScreamingDelegate(callback: { [weak manager] result in
+            manager?.stopUpdatingLocation()
+            self.parentViewController.unretain(retainId)
+        })
+        manager.delegate = sd
+        manager.retain(as: "delegate", item: sd)
+    }
+    
+    private class ScreamingDelegate: NSObject, CLLocationManagerDelegate {
+        let callback: (LocationResult)->Void
+        
+        init(callback: @escaping (LocationResult)->Void) {
+            self.callback = callback
+        }
+        
+        func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+            //GOTTEM
+            if let didUpdateToLocation = locations.last {
+                callback(LocationResult(
+                    latitude: didUpdateToLocation.coordinate.latitude,
+                    longitude: didUpdateToLocation.coordinate.longitude,
+                    accuracyMeters: didUpdateToLocation.horizontalAccuracy,
+                    altitudeMeters: didUpdateToLocation.altitude,
+                    altitudeAccuracyMeters: didUpdateToLocation.verticalAccuracy,
+                    headingFromNorth: didUpdateToLocation.course + 90,
+                    speedMetersPerSecond: didUpdateToLocation.speed
+                ))
+            }
+        }
+        
+        func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+            if status == CLAuthorizationStatus.authorizedWhenInUse || status == CLAuthorizationStatus.authorizedAlways {
+                if CLLocationManager.locationServicesEnabled() {
+                    manager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+                    manager.startUpdatingLocation()
+                }
+            }
+        }
+    }
+}
