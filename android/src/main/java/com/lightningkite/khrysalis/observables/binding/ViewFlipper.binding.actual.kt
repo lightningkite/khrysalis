8c143ab27ea32f768e1f1ffeff607de0f389d5c2
Brady <brady@lightningkite.com>
2020-02-12 16:09:16 -0700

    Added a color option to the progress bar spinner. It doesn't work, and only gives white when used. But the option is there with a todo to fix later.

--- android/src/main/java/com/lightningkite/khrysalis/observables/binding/ViewFlipper.binding.actual.kt
+++ android/src/main/java/com/lightningkite/khrysalis/observables/binding/ViewFlipper.binding.actual.kt
@@ -1,5 +1,10 @@
+@file:Suppress("NAME_SHADOWING")
+
 package com.lightningkite.khrysalis.observables.binding
 
+import android.graphics.Color
+import android.graphics.LightingColorFilter
+import android.graphics.PorterDuff
 import android.view.Gravity
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
@@ -10,9 +15,11 @@ import android.widget.CompoundButton
 import android.widget.FrameLayout
 import android.widget.ProgressBar
 import android.widget.ViewFlipper
+import androidx.core.content.res.ResourcesCompat
 import com.lightningkite.khrysalis.observables.*
+import com.lightningkite.khrysalis.views.ColorResource
 
-fun ViewFlipper.bindLoading(loading: MutableObservableProperty<Boolean>) {
+fun ViewFlipper.bindLoading(loading: MutableObservableProperty<Boolean>, color: ColorResource? = null) {
     if (this.inAnimation == null)
         this.inAnimation = AlphaAnimation(0f, 1f).apply {
             duration = 250
@@ -24,7 +31,13 @@ fun ViewFlipper.bindLoading(loading: MutableObservableProperty<Boolean>) {
             interpolator = LinearInterpolator()
         }
     if (this.childCount == 1) {
-        addView(ProgressBar(context), 1, FrameLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT, Gravity.CENTER))
+        val spinner = ProgressBar(context)
+        color?.let{ color ->
+            //TODO: Make this actually show the proper color. Currently it only makes it white.
+            val colorValue = resources.getColor(color)
+            spinner.indeterminateDrawable.colorFilter = LightingColorFilter( 0xFFFFFFFF.toInt() - colorValue, colorValue)
+        }
+        addView(spinner, 1, FrameLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT, Gravity.CENTER))
     }
     loading.addAndRunWeak(this) { self, it ->
         self.displayedChild = if (it) 1 else 0
