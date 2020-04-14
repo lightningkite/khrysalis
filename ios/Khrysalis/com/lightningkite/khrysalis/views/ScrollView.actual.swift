e62273a3c478088a5632372bb4485425c7fb69f6
Brady Svedin <brady@lightningkite.com>
2020-04-14 11:16:05 -0600

    Swift scrollview
    Added scroll to bottom for UIScrollView

--- ios/Khrysalis/com/lightningkite/khrysalis/views/ScrollView.actual.swift
+++ ios/Khrysalis/com/lightningkite/khrysalis/views/ScrollView.actual.swift
@@ -20,5 +20,9 @@ public extension UIScrollView {
     var scrollY: Int32 {
         return Int32(self.contentOffset.y)
     }
+    func scrollToBottom(){
+        let bottomOffset = CGPoint(x: 0, y: self.contentSize.height - self.bounds.size.height)
+        self.setContentOffset(bottomOffset, animated: true)
+    }
 }
 
