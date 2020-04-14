5449a2c8ff42aa06a933a32aaeb38ef19cb6620b
Brady <brady@lightningkite.com>
2020-04-14 11:15:32 -0600

    Added scrollview extension scroll to bottom.

---
+++ android/src/main/java/com/lightningkite/khrysalis/ScrollView.extensions.actual.kt
@@ -0,0 +1,10 @@
+package com.lightningkite.khrysalis
+
+import android.widget.ScrollView
+
+fun ScrollView.scrollToBottom() {
+    val lastChild = getChildAt(childCount - 1)
+    val bottom = lastChild.bottom + paddingBottom
+    val delta = bottom - (scrollY+ height)
+    smoothScrollBy(0, delta)
+}
