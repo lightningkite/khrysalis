77a90fffd0c4665d9e987df1ba001875efa26a48
Brady <brady@lightningkite.com>
2020-04-15 17:26:39 -0600

    removed dup file

--- plugin/src/main/kotlin/com/lightningkite/khrysalis/web/typescript/lambda.kt
+++ plugin/src/main/kotlin/com/lightningkite/khrysalis/web/typescript/lambda.kt
@@ -13,4 +13,16 @@ fun TypescriptTranslator.registerLambda() {
         -typedRule.statements()
         -"}"
     }
+
+    this.handle<KotlinParser.AnonymousFunctionContext> {
+        -typedRule.parametersWithOptionalType()
+        typedRule.type()?.lastOrNull()?.let {
+            -": "
+            -it
+        }
+        -" => "
+        -typedRule.functionBody()?.block()
+        -typedRule.functionBody()?.expression()
+        -"\n"
+    }
 }
