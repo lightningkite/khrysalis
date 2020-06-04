d2db09dc281c2cb045aae3aa131aa0c6cf602158
Brady Svedin <brady@lightningkite.com>
2020-06-04 16:28:46 -0600

    Added methods on ViewDependency to get videos from gallery and record videos.

--- android/src/main/java/com/lightningkite/khrysalis/views/ViewDependency.actual.kt
+++ android/src/main/java/com/lightningkite/khrysalis/views/ViewDependency.actual.kt
@@ -198,6 +198,138 @@ fun ViewDependency.requestImageCamera(
 }
 
 
+fun ViewDependency.requestVideoGallery(
+    callback: (Uri) -> Unit
+) {
+    requestPermission(Manifest.permission.READ_EXTERNAL_STORAGE) {
+        if (it) {
+            val getIntent = Intent(Intent.ACTION_GET_CONTENT)
+            getIntent.type = "video/*"
+
+            val pickIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            pickIntent.type = "video/*"
+
+            val chooserIntent = Intent.createChooser(getIntent, "Select Video")
+            chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, arrayOf(pickIntent))
+
+            this.startIntent(chooserIntent) { code, result ->
+                val uri = result?.data
+                if (code == Activity.RESULT_OK && uri != null) {
+                    callback(uri)
+                }
+            }
+        }
+    }
+}
+
+fun ViewDependency.requestVideosGallery(
+    callback: (List<Uri>) -> Unit
+) {
+    requestPermission(Manifest.permission.READ_EXTERNAL_STORAGE) {
+        if (it) {
+            val getIntent = Intent(Intent.ACTION_GET_CONTENT)
+            getIntent.type = "video/*"
+            getIntent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+
+            val pickIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            pickIntent.type = "video/*"
+
+            val chooserIntent = Intent.createChooser(getIntent, "Select Video")
+            chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, arrayOf(pickIntent))
+
+            this.startIntent(chooserIntent) { code, result ->
+                if (code == Activity.RESULT_OK) {
+                    result?.clipData?.let { clipData ->
+                        callback((0 until clipData.itemCount).map { index -> clipData.getItemAt(index).uri })
+                    } ?: result?.data?.let { callback(listOf(it)) }
+                }
+            }
+        }
+    }
+}
+
+
+fun ViewDependency.requestVideoCamera(
+    front: Boolean = false,
+    callback: (Uri) -> Unit
+) {
+    val fileProviderAuthority = context.packageName + ".fileprovider"
+    val file = File(context.cacheDir, "videos").also { it.mkdirs() }
+        .let { File.createTempFile("video", ".mp4", it) }
+        .let { FileProvider.getUriForFile(context, fileProviderAuthority, it) }
+    requestPermission(Manifest.permission.CAMERA) {
+        if (it) {
+            val intent = Intent(MediaStore.ACTION_VIDEO_CAPTURE)
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, file)
+            //TODO:Test this on an older device. This works on newest, but we need to make sure it works/doesn't crash a newer one.
+            if (front) {
+//                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
+
+                intent.putExtra("android.intent.extras.LENS_FACING_FRONT", 1)
+//                }else{
+                intent.putExtra("android.intent.extras.CAMERA_FACING", 1)
+
+//                }
+                intent.putExtra("android.intent.extra.USE_FRONT_CAMERA", true)
+            }
+            startIntent(intent) { code, result ->
+                if (code == Activity.RESULT_OK) callback(result?.data ?: file)
+            }
+        }
+    }
+}
+
+
+fun ViewDependency.requestMediasGallery(
+    callback: (List<Uri>) -> Unit
+) {
+    requestPermission(Manifest.permission.READ_EXTERNAL_STORAGE) {
+        if (it) {
+            val getIntent = Intent(Intent.ACTION_GET_CONTENT)
+            getIntent.type = "video/* image/*"
+            getIntent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+
+            val pickIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            pickIntent.type = "video/* image/*"
+
+            val chooserIntent = Intent.createChooser(getIntent, "Select Media")
+            chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, arrayOf(pickIntent))
+
+            this.startIntent(chooserIntent) { code, result ->
+                if (code == Activity.RESULT_OK) {
+                    result?.clipData?.let { clipData ->
+                        callback((0 until clipData.itemCount).map { index -> clipData.getItemAt(index).uri })
+                    } ?: result?.data?.let { callback(listOf(it)) }
+                }
+            }
+        }
+    }
+}
+fun ViewDependency.requestMediaGallery(
+    callback: (Uri) -> Unit
+) {
+    requestPermission(Manifest.permission.READ_EXTERNAL_STORAGE) {
+        if (it) {
+            val getIntent = Intent(Intent.ACTION_GET_CONTENT)
+            getIntent.type = "video/* image/*"
+
+            val pickIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            pickIntent.type = "video/* image/*"
+
+            val chooserIntent = Intent.createChooser(getIntent, "Select Media")
+            chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, arrayOf(pickIntent))
+
+            this.startIntent(chooserIntent) { code, result ->
+                val uri = result?.data
+                if (code == Activity.RESULT_OK && uri != null) {
+                    callback(uri)
+                }
+            }
+        }
+    }
+}
+
+
 @Deprecated("")
 fun ViewDependency.downloadDrawable(
     url: String,
