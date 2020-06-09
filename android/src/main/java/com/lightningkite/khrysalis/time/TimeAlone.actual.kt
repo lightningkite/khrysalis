595b3943fc8569e2608233787fc0b367a0f9f2a0
Brady Svedin <brady@lightningkite.com>
2020-06-09 13:45:27 -0600

    Modified interval function calls to use secondsInDay rather than duplicating the logic.

--- android/src/main/java/com/lightningkite/khrysalis/time/TimeAlone.actual.kt
+++ android/src/main/java/com/lightningkite/khrysalis/time/TimeAlone.actual.kt
@@ -47,7 +47,7 @@ operator fun TimeAlone.minus(rhs: TimeAlone): TimeInterval {
 
 operator fun TimeAlone.minus(rhs: TimeInterval): TimeAlone {
     val result =
-        (this.hour * 60 * 60 + this.minute * 60 + this.second) - rhs.milliseconds.toInt() / 1000
+        this.secondsInDay - rhs.milliseconds.toInt() / 1000
     return if (result < 0) {
         TimeAlone(0, 0, 0)
     } else {
@@ -57,6 +57,6 @@ operator fun TimeAlone.minus(rhs: TimeInterval): TimeAlone {
 
 operator fun TimeAlone.plus(rhs: TimeInterval): TimeAlone {
     val result =
-        (this.hour * 60 * 60 + this.minute * 60 + this.second) + rhs.milliseconds.toInt() / 1000
+        this.secondsInDay + rhs.milliseconds.toInt() / 1000
     return TimeAlone(result / 60 / 60, result / 60 % 60, result % 60)
 }
