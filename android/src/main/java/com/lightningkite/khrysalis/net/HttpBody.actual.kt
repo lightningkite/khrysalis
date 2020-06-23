c1a3e212de0dc00c098a89cc72890bf707b8ae28
Joseph Ivie <joseph@lightningkite.com>
2020-06-23 16:54:05 -0600

    Uri to body

--- android/src/main/java/com/lightningkite/khrysalis/net/HttpBody.actual.kt
+++ android/src/main/java/com/lightningkite/khrysalis/net/HttpBody.actual.kt
@@ -5,9 +5,13 @@ import android.util.Log
 import com.lightningkite.khrysalis.Codable
 import com.lightningkite.khrysalis.IsCodable
 import com.lightningkite.khrysalis.Image
+import com.lightningkite.khrysalis.Uri
 import com.lightningkite.khrysalis.bytes.Data
+import okhttp3.MediaType
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
+import okio.BufferedSink
+import okio.Okio
 import java.io.ByteArrayOutputStream
 
 
@@ -44,12 +48,29 @@ fun Bitmap.toHttpBody(maxBytes: Long = 10_000_000): HttpBody {
     return RequestBody.create(HttpMediaTypes.JPEG, data)
 }
 
+fun Uri.toHttpBody(type: HttpMediaType): HttpBody {
+    return object : RequestBody() {
+        override fun contentType(): MediaType = type
+
+        override fun writeTo(sink: BufferedSink) {
+            HttpClient.appContext.contentResolver.openInputStream(this@toHttpBody)?.use {
+                Okio.source(it).use { source ->
+                    sink.writeAll(source)
+                }
+            } ?: throw IllegalStateException("URI (${this@toHttpBody}) could not be opened")
+        }
+
+    }
+}
+
 fun multipartFormBody(vararg parts: HttpBodyPart): HttpBody {
     return MultipartBody.Builder().setType(MultipartBody.FORM).also {
-        for(part in parts){
+        for (part in parts) {
             it.addPart(part)
         }
     }.build()
 }
+
 fun multipartFormFilePart(name: String, value: String): HttpBodyPart = HttpBodyPart.createFormData(name, value)
-fun multipartFormFilePart(name: String, filename: String? = null, body: HttpBody): HttpBodyPart = HttpBodyPart.createFormData(name, filename, body)
+fun multipartFormFilePart(name: String, filename: String? = null, body: HttpBody): HttpBodyPart =
+    HttpBodyPart.createFormData(name, filename, body)
--- android/src/main/java/com/lightningkite/khrysalis/net/HttpMediaType.actual.kt
+++ android/src/main/java/com/lightningkite/khrysalis/net/HttpMediaType.actual.kt
@@ -8,4 +8,5 @@ object HttpMediaTypes {
     val JSON = MediaType.parse("application/json")!!
     val TEXT = MediaType.parse("text/plain")!!
     val JPEG = MediaType.parse("image/jpeg")!!
+    fun fromString(text: String) = MediaType.parse(text)
 }
