b81b4d47f936541ebb3a4e7e002dfc8f215e21c5
Brady Svedin <brady@lightningkite.com>
2020-06-26 14:51:20 -0600

    Added tablayout bind where tabs is an observable as well.

--- android/src/main/java/com/lightningkite/khrysalis/observables/binding/TabLayout.binding.actual.kt
+++ android/src/main/java/com/lightningkite/khrysalis/observables/binding/TabLayout.binding.actual.kt
@@ -50,3 +50,53 @@ fun TabLayout.bind(
     })
 }
 
+
+/**
+ *
+ * Binds the tabs as well as the selected tab to the data provided.
+ * tabs is the title of each tab and it will create a tab for each of them.
+ * selected is the tab number that will be selected. User selecting a tab
+ * will update selected, as well modifying selected will manifest in the tabs.
+ *
+ */
+
+fun TabLayout.bind(
+    tabs: ObservableProperty<List<String>>,
+    selected: MutableObservableProperty<Int>,
+    allowReselect:Boolean = false
+) {
+    tabs.subscribeBy{tabs ->
+        this.removeAllTabs()
+        for (tab in tabs) {
+            addTab(newTab().setText(tab))
+        }
+    }.until(this.removed)
+
+    selected.subscribeBy { value ->
+        this.getTabAt(value)?.select()
+    }.until(this.removed)
+    this.addOnTabSelectedListener(object : TabLayout.BaseOnTabSelectedListener<TabLayout.Tab> {
+
+        var suppress = false
+
+        override fun onTabReselected(p0: TabLayout.Tab) {
+            if (!suppress && allowReselect) {
+                suppress = true
+                selected.value = p0.position
+                suppress = false
+            }
+        }
+
+        override fun onTabUnselected(p0: TabLayout.Tab) {}
+
+        override fun onTabSelected(p0: TabLayout.Tab) {
+            if (!suppress) {
+                suppress = true
+                if (selected.value != p0.position)
+                    selected.value = p0.position
+                suppress = false
+            }
+        }
+    })
+}
+
