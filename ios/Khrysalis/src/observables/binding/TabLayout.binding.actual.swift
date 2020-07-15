693707c3fe3afeb7f11fb75b11f97bebd97ea8c9
Joseph Ivie <joseph@lightningkite.com>
2020-07-15 13:02:46 -0600

    Tab bind fix

--- ios/Khrysalis/src/observables/binding/TabLayout.binding.actual.swift
+++ ios/Khrysalis/src/observables/binding/TabLayout.binding.actual.swift
@@ -6,6 +6,7 @@ import UIKit
 //--- TabLayout.bind(List<String>, MutableObservableProperty<Int>)
 public extension UISegmentedControl {
     func bind(_ tabs: Array<String>, _ selected: MutableObservableProperty<Int>) -> Void {
+        self.removeAllSegments()
         for entry in tabs {
             self.insertSegment(withTitle: entry, at: self.numberOfSegments, animated: false)
         }
@@ -14,6 +15,21 @@ public extension UISegmentedControl {
         })
         selected.subscribeBy { value in
             self.selectedSegmentIndex = Int(value)
+        }.until(self.removed)
+    }
+    func bind(tabs: Array<String>, selected: MutableObservableProperty<Int>) -> Void {
+        return bind(tabs, selected)
+    }
+    func bind<T: Equatable>(_ tabs: Array<T>, _ selected: MutableObservableProperty<T>, _ toString: @escaping (T)->String) -> Void {
+        self.removeAllSegments()
+        for entry in tabs {
+            self.insertSegment(withTitle: toString(entry), at: self.numberOfSegments, animated: false)
+        }
+        self.addAction(for: .valueChanged, action: { [weak self, weak selected] in
+            selected?.value = tabs[self?.selectedSegmentIndex ?? 0]
+        })
+        selected.subscribeBy { value in
+            self.selectedSegmentIndex = tabs.index(of: value)
         }.until(self.removed)
     }
     func bind(tabs: Array<String>, selected: MutableObservableProperty<Int>) -> Void {
