fcd17945cee3fbe173dd220a46e44aa62954c410
Brady Svedin <brady@lightningkite.com>
2022-02-02 11:19:20 -0700

    Small Gradle Update

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlinVersion")
         classpath("org.jetbrains.dokka:dokka-gradle-plugin:$kotlinVersion")
-        classpath("com.lightningkite:deploy-helpers:master-SNAPSHOT")
+        classpath("com.lightningkite:deploy-helpers:0.0.4")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
