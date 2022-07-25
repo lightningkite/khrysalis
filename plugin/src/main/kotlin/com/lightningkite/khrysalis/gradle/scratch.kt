28adbc71a76845002144a559e746f77ef2c3a7a5
Joseph Ivie <joseph@lightningkite.com>
2022-07-25 15:28:01 -0600

    Fix for translations in progress

--- plugin/src/main/kotlin/com/lightningkite/khrysalis/gradle/scratch.kt
+++ plugin/src/main/kotlin/com/lightningkite/khrysalis/gradle/scratch.kt
@@ -7,5 +7,13 @@ import org.gradle.api.Project
 import org.gradle.api.file.SourceDirectorySet
 import org.gradle.api.tasks.SourceSet
 import org.gradle.language.base.plugins.LanguageBasePlugin
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmOptions
+import org.jetbrains.kotlin.gradle.tasks.AbstractKotlinCompile
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.util.*
+
+//fun <T> Project.duplicate(newName: String, kotlinCompile: KotlinCompile): KotlinCompile {
+//    return KotlinCompile(
+//        KotlinJvmOptions
+//    )
+//}
