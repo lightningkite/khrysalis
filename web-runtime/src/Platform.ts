fbc31e3a062558d019fa63fac441a5a044cc9fb9
Brady Svedin <brady@lightningkite.com>
2023-08-24 12:55:02 -0600

    Fixed web runtime Platform Current issue.

--- web-runtime/src/Platform.ts
+++ web-runtime/src/Platform.ts
@@ -5,5 +5,5 @@ export enum Platform {
 export namespace Platform {
     export const Companion = Platform
     export const INSTANCE = Platform
-    export const current = Platform.Web
+    export const current:Platform = Platform.Web
 }
